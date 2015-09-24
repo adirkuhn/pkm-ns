3c7e973fb07cb69b8e36a68aa33b3988a567dfab
Adir Kuhn <adirkuhn@gmailcom>
2015-09-24 18:37:52 -0300

    Creating App ViewModel

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
 var application = require("application");
-application.mainModule = "main-page";
+application.mainModule = "views/main/main-page";
 application.cssFile = "./app.css";
 application.start();
