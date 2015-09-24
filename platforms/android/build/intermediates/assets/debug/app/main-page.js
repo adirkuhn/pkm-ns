a7ee6a23e073b71877c9171b6edaaab4ec9252d6
Adir Kuhn <adirkuhn@gmail.com>
2015-09-24 16:44:18 -0300

    Initial NativeScript Project

---
+++ platforms/android/build/intermediates/assets/debug/app/main-page.js
@@ -0,0 +1,7 @@
+var vmModule = require("./main-view-model");
+
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = vmModule.mainViewModel;
+}
+exports.pageLoaded = pageLoaded;
